99cab1a6e82e301aa39f5d704caf8b8316356c8a
Shashwat Anand <shashwat.anand@gmail.com>
2014-10-11 08:50:12 +0530

    Updated the size of application and launcch of editor

--- plugins/com.shashwat.opengl.firstdemo/src/com/shashwat/opengl/firstdemo/ApplicationWorkbenchWindowAdvisor.java
+++ plugins/com.shashwat.opengl.firstdemo/src/com/shashwat/opengl/firstdemo/ApplicationWorkbenchWindowAdvisor.java
@@ -1,10 +1,15 @@
 package com.shashwat.opengl.firstdemo;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+import com.shashwat.opengl.firstdemo.parts.editor.OpenGLEditor;
+import com.shashwat.opengl.firstdemo.parts.editor.OpenGLEditorInput;
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -18,8 +23,18 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
     
     public void preWindowOpen() {
         IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
-        configurer.setInitialSize(new Point(800, 600));
+        configurer.setInitialSize(new Point(1200, 600));
         configurer.setShowCoolBar(false);
         configurer.setShowStatusLine(false);
+    }
+    
+    @Override
+    public void postWindowOpen() {
+    	IWorkbenchWindow activeWorkbenchWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+    	try {
+    		activeWorkbenchWindow.getActivePage().openEditor(new OpenGLEditorInput(), OpenGLEditor.ID);
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
     }
 }
